db5c06020c669eb4759519f15bd991d91b84ff74
Bartek Mroczkowski <bar.mroczkowski@gmail.com>
2023-11-18 13:10:42 +0100

    #mapCreate v0.4

--- Assets/Scripts/SpellCast.cs
+++ Assets/Scripts/SpellCast.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Data.SqlTypes;
 using UnityEngine;
 using static UnityEngine.RuleTile.TilingRuleOutput;
 
@@ -10,35 +11,34 @@ public class FireballController : MonoBehaviour
     [SerializeField] private float _lifeTime = 2f;
     [SerializeField] private float _cooldawnSpell = 2f;
     [SerializeField] private float _forcePunch = 20;
-    Rigidbody2D rb2d;
-    float delayTime = 0.5f;
+    
+    private Rigidbody2D _fireballRigidbody;
+    private float delayTime = 0.5f;
+    private bool _readyToCast;
 
-    Vector2 directionOne;
     void Start()
     {
-        rb2d = gameObject.AddComponent<Rigidbody2D>();
-        rb2d.gravityScale = 0;
-        rb2d.freezeRotation = true;
+        _fireballRigidbody = gameObject.AddComponent<Rigidbody2D>();
+        _fireballRigidbody.gravityScale = 0;
+        _fireballRigidbody.freezeRotation = true;
 
-        
+        _readyToCast = false;
         StartCoroutine(StartMovementAfterDelay());
     }
     IEnumerator StartMovementAfterDelay()
     {
+        yield return new WaitForSeconds(delayTime);
+        
         Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         mousePosition.z = 0f;
-
         Vector2 direction = (mousePosition - transform.position).normalized;
-
-        Vector2 directionOne = new Vector2(direction.x/(Mathf.Sqrt(Mathf.Pow(direction.x,2) + Mathf.Pow(direction.y, 2))), direction.y/ (Mathf.Sqrt(Mathf.Pow(direction.x, 2) + Mathf.Pow(direction.y, 2))));
+        Vector2 unitVector = new Vector2(direction.x/(Mathf.Sqrt(Mathf.Pow(direction.x,2) + Mathf.Pow(direction.y, 2))), direction.y/ (Mathf.Sqrt(Mathf.Pow(direction.x, 2) + Mathf.Pow(direction.y, 2))));
 
         float angle = Mathf.Atan2(direction.y,direction.x) * Mathf.Rad2Deg + 90;
-        transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
-
-        yield return new WaitForSeconds(delayTime);
-
-        rb2d.velocity = directionOne * _fireballSpeed;
-
+        transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);    
+        
+        _readyToCast = true;
+        _fireballRigidbody.velocity = unitVector * _fireballSpeed;
         Destroy(gameObject, _lifeTime);
     }
     private void OnTriggerEnter2D(Collider2D collision)
@@ -49,11 +49,11 @@ public class FireballController : MonoBehaviour
             Rigidbody2D enemyRb = collision.GetComponent<Rigidbody2D>();
             if (enemyRb != null)
             {
-                enemyRb.AddForce(rb2d.velocity/_fireballSpeed * _forcePunch, ForceMode2D.Impulse);
+                enemyRb.AddForce(_fireballRigidbody.velocity/_fireballSpeed * _forcePunch, ForceMode2D.Impulse);
                 StartCoroutine(StopEnemy(enemyRb, 0.1f));
             }
             
-            rb2d.velocity = Vector2.zero;
+            _fireballRigidbody.velocity = Vector2.zero;
             _animator.SetTrigger("Destroy");
             Destroy(gameObject, 1f);
         }
@@ -64,6 +64,13 @@ public class FireballController : MonoBehaviour
         enemyRb.velocity = Vector2.zero;
     }
 
-    void Update(){  }
+    void Update(){
+
+        if (_readyToCast == false)
+        {
+            _fireballRigidbody.transform.position = GameObject.Find("Player").transform.position; 
+        }
+    
+    }
 }
 
